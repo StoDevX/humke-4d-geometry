829609166a2f8f9b5237746f2b8e0a9d08a4c185
peters10 <peters10@stolaf.edu>
2017-09-21 00:47:27 -0500

    2d grid and controls

--- src/2d.js
+++ src/2d.js
@@ -23,10 +23,12 @@ var Mode2D = (function (scope) {
 		this.leftView = null;
 		this.leftCamera = null;
 		this.leftRenderer = null;
+		this.leftControls = null;
 
 		this.rightView = null;
 		this.rightCamera = null;
 		this.rightRenderer = null;
+		this.rightControls = null;
 
 	}
 
@@ -43,8 +45,6 @@ var Mode2D = (function (scope) {
 		this.leftChild = leftChild; this.rightChild = rightChild;
 
 		var viewWidth = (window.innerWidth-20)/2;
-		//var leftView = this.createView(leftChild,viewWidth);
-		//var rightView = this.createView(rightChild,viewWidth);
 
 		// Init gui
 		gui.init("2D",this.callbacks,this);
@@ -52,84 +52,43 @@ var Mode2D = (function (scope) {
 
 		this.leftView = new THREE.Scene();
 		this.leftCamera = new THREE.PerspectiveCamera( 75, viewWidth / window.innerHeight, 0.1, 1000 );
+		this.leftCamera.position.set(0,0,20);
 		this.leftRenderer = new THREE.WebGLRenderer();
 		this.leftRenderer.setClearColor(0xffffff);
 		this.leftRenderer.setSize( viewWidth, window.innerHeight );
 		leftChild.appendChild( this.leftRenderer.domElement );
+
+		this.leftControls = new THREE.OrbitControls( this.leftCamera, this.leftRenderer.domElement );
+		this.leftControls.enableRotate = false;
+
+		var grid = this.createGrid(20,20,Math.PI/2)
+		this.leftView.add(grid);
 
 		var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 		var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 		var cube = new THREE.Mesh( geometry, material );
 		this.leftView.add( cube );
 
-		this.leftCamera.position.z = 5;
-
 		this.rightView = new THREE.Scene();
 		this.rightCamera = new THREE.PerspectiveCamera( 75, viewWidth / window.innerHeight, 0.1, 1000 );
+		this.rightCamera.position.set(0,0,20);
 		this.rightRenderer = new THREE.WebGLRenderer();
 		this.rightRenderer.setClearColor(0xffffff);
 		this.rightRenderer.setSize( viewWidth, window.innerHeight );
 		rightChild.appendChild( this.rightRenderer.domElement );
+
+		this.rightControls = new THREE.OrbitControls( this.rightCamera, this.rightRenderer.domElement );
+		this.rightControls.enableRotate = false;
 
 		geometry = new THREE.BoxGeometry( 1, 1, 1 );
 		material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 		cube = new THREE.Mesh( geometry, material );
 		this.rightView.add( cube );
 
-		this.rightCamera.position.z = 5;
-
 		this.animate();
-		// Set up left view
-		// var camera = leftView.camera({
-		// 	proxy: true, // this alows interactive camera controls to override the position
-		// 	position: [0, 0, 3],
-		// })
-		// leftView = leftView.cartesian({
-		// 	range: [[-10, 10], [-10, 10]],
-		// 	scale: [1, 1],
-		// });
-		// leftView
-		// .axis({
-		// 	axis: 1,
-		// 	width: 4,
-		// 	color:'black',
-		// })
-		// .axis({
-		// 	axis: 2,
-		// 	width: 4,
-		// 	color:'black',
-		// })
-		// .grid({
-		// 	width: 1,
-		// 	divideX: 10,
-		// 	divideY: 10
-		// });
-		//
-		// // Add text
-		// leftView.array({
-		// 	data: [[11,1], [0,12]],
-		// 	channels: 2, // necessary
-		// 	live: false,
-		// }).text({
-		// 	data: ["x", "y"],
-		// }).label({
-		// 	color: 0x000000,
-		// });
-		//
-		// this.leftView = leftView;
+
 		// this.CreateViewLine();
 
-		// // Set up right view
-		// rightView = rightView.cartesian({
-		// 	range: [[-10, 10],[-10,10]],
-		// 	scale: [1, 1],
-		// });
-		// rightView.camera({
-		// 	proxy: true, // this alows interactive camera controls to override the position
-		// 	position: [0, 0, 3],
-		// })
-		// this.rightView = rightView
-		//
 		// this.axis_object  = this.CreateViewAxis(this.rightView,1,[11,1],"x")
 		//
 		// // Set up right view intersection shader
@@ -211,6 +170,12 @@ var Mode2D = (function (scope) {
 		})
 		.fragment()
 
+	}
+
+	Mode2D.prototype.createGrid = function(size, divisions, xRotation) {
+		var gridHelper = new THREE.GridHelper(size,divisions,0x444444,0xd0d0d0);
+		gridHelper.rotation.x = xRotation;
+		return gridHelper;
 	}
 
 	Mode2D.prototype.CreateViewAxis = function(view,axisNum,pos,labelName){
@@ -739,7 +704,9 @@ Mode2D.prototype.convertToPixels = function(geom_func,id){
 
 Mode2D.prototype.animate = function(){
 	requestAnimationFrame( this.animate.bind(this) );
+
 	this.leftRenderer.render( this.leftView, this.leftCamera );
+
 	this.rightRenderer.render( this.rightView, this.rightCamera );
 }
 
