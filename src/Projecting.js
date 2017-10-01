c9f8b6c18d1821017cc0e0e2afff5ac6db9d2f66
Omar Shehata <shehat1@stolaf.edu>
2017-09-30 21:24:04 -0500

    Moved intersection code to Slicing.js

---
+++ src/Projecting.js
@@ -0,0 +1,13 @@
+/*
+The projecting class contains pure functions for computing and rendering 
+anything related to the left side of the screen (projection n dimensional objects)
+*/
+var Projecting = (function (scope) {
+	function Projecting(){
+		
+	}
+	
+
+	scope.Projecting = Projecting;
+	return Projecting;
+})(typeof exports === 'undefined' ? {} : exports);
