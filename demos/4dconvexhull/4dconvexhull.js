d90739cd7eeb788233b4f8583da3b5a359c46b2e
Justin Pacholec <pachol1@stolaf.edu>
2017-11-08 11:37:15 -0600

    updating 4dconvexhull to be a class

--- demos/4dconvexhull/4dconvexhull.js
+++ demos/4dconvexhull/4dconvexhull.js
@@ -1,7 +1,7 @@
 var ConvexHull4D = (function (scope) {
 	function ConvexHull4D(){}
 
-  ClassName.prototype.arrEqual = function(arr1, arr2) {
+  ConvexHull4D.prototype.arrEqual = function(arr1, arr2) {
     if (arr1.length != arr2.length) {
       return false;
     }
@@ -11,19 +11,19 @@ var ConvexHull4D = (function (scope) {
     return true;
   }
 
-  ClassName.prototype.Arr1InArr2 = function(arr1, arr2) {
+  ConvexHull4D.prototype.Arr1InArr2 = function(arr1, arr2) {
     for (var arr2_i = 0; arr2_i < arr2.length; arr2_i++) {
-      if (arrEqual(arr1, arr2[arr2_i])) return true;
+      if (this.arrEqual(arr1, arr2[arr2_i])) return true;
     }
     return false;
   }
 
-  ClassName.prototype.CreateInitialSimplex = function() {
+  ConvexHull4D.prototype.CreateInitialSimplex = function() {
     // Create an initial simplex of d+1 points and return its facets
     return [[1,2,3,4],[4,3,2,0],[0,1,3,4],[4,2,1,0],[0,1,2,3]];
   }
 
-  ClassName.prototype.vecSubtract = function(A, B) {
+  ConvexHull4D.prototype.vecSubtract = function(A, B) {
     var result = [];
     for (var i = 0; i < A.length; i++) {
       result.push(A[i] - B[i]);
@@ -31,7 +31,7 @@ var ConvexHull4D = (function (scope) {
     return result;
   }
 
-  ClassName.prototype.Cross = function(A,B,C) {
+  ConvexHull4D.prototype.Cross = function(A,B,C) {
     // Calculate the normal vector to the hyperplane defined by vectors A, B, and C.
     // Values taken from a 4x4 determinant calculated by hand.
     // For more info, see the "multilinear algebra" section of
@@ -44,7 +44,7 @@ var ConvexHull4D = (function (scope) {
     return [e0, e1, e2, e3];
   }
 
-  ClassName.prototype.Dot = function(A,B) {
+  ConvexHull4D.prototype.Dot = function(A,B) {
   // The dot product of vectors A and B
 
     var dot_product = 0;
@@ -54,7 +54,7 @@ var ConvexHull4D = (function (scope) {
     return dot_product;
   }
 
-  ClassName.prototype.IsPointAboveHyperplane = function(normal, p0, point) {
+  ConvexHull4D.prototype.IsPointAboveHyperplane = function(normal, p0, point) {
     // Take a normal vector and a point p0 that define a hyperplane
     // and determine if a point is above or below that hyperplane
 
@@ -62,26 +62,26 @@ var ConvexHull4D = (function (scope) {
     for (var i = 0; i < 4; i++) {
       vec.push(point[i] - p0[i]);
     }
-    return Dot(normal, vec) < 0;
+    return this.Dot(normal, vec) < 0;
   }
 
-  ClassName.prototype.GetHyperplaneOfFacet = function(facet, points) {
+  ConvexHull4D.prototype.GetHyperplaneOfFacet = function(facet, points) {
     // Return the normal and point representing the hyperplane that a facet lies on
 
-    var v1 = vecSubtract(points[facet[1]], points[facet[0]]);
-    var v2 = vecSubtract(points[facet[2]], points[facet[1]]);
-    var v3 = vecSubtract(points[facet[3]], points[facet[2]]);
-    var normal = Cross(v1, v2, v3);
+    var v1 = this.vecSubtract(points[facet[1]], points[facet[0]]);
+    var v2 = this.vecSubtract(points[facet[2]], points[facet[1]]);
+    var v3 = this.vecSubtract(points[facet[3]], points[facet[2]]);
+    var normal = this.Cross(v1, v2, v3);
     var p0 = points[facet[0]];
     return [normal, p0];
   }
 
-  ClassName.prototype.IsPointAboveFacet = function(facet_index, point_index, hull) {
-    var hyperplane = GetHyperplaneOfFacet(hull.facets[facet_index], hull.points);
-    return IsPointAboveHyperplane(hyperplane[0], hyperplane[1], hull.points[point_index]);
+  ConvexHull4D.prototype.IsPointAboveFacet = function(facet_index, point_index, hull) {
+    var hyperplane = this.GetHyperplaneOfFacet(hull.facets[facet_index], hull.points);
+    return this.IsPointAboveHyperplane(hyperplane[0], hyperplane[1], hull.points[point_index]);
   }
 
-  ClassName.prototype.GetInitialOutsideSets = function(hull) {
+  ConvexHull4D.prototype.GetInitialOutsideSets = function(hull) {
     // for each facet F
     //   for each unassigned point p
     //     if p is above F
@@ -91,7 +91,7 @@ var ConvexHull4D = (function (scope) {
     for (var fi = 0; fi < hull.facets.length; fi++) {
       outside_sets.push([]);
       for (var pi = 5; pi < hull.points.length; pi++) {
-        if (IsPointAboveFacet(fi, pi, hull)) {
+        if (this.IsPointAboveFacet(fi, pi, hull)) {
           outside_sets[fi].push(pi);
         }
       }
@@ -99,7 +99,7 @@ var ConvexHull4D = (function (scope) {
     return outside_sets;
   }
 
-  ClassName.prototype.Magnitude = function(vec) {
+  ConvexHull4D.prototype.Magnitude = function(vec) {
     var sum = 0;
     for (var vi = 0; vi < vec.length; vi++) {
       sum += Math.pow(vec[vi], 2);
@@ -107,31 +107,31 @@ var ConvexHull4D = (function (scope) {
     return Math.sqrt(sum);
   }
 
-  ClassName.prototype.DistanceFromHyperplane = function(normal, p0, point) {
+  ConvexHull4D.prototype.DistanceFromHyperplane = function(normal, p0, point) {
     // Calculate the distance of a point from a hyperplane.
     // For more information, see https://math.stackexchange.com/questions/1210545/distance-from-a-point-to-a-hyperplane
 
-    var b = Dot(normal, vecSubtract([0,0,0,0],p0));
-    var numerator = Math.abs(Dot(normal, point) + b);
-    var denominator = Magnitude(normal);
+    var b = this.Dot(normal, this.vecSubtract([0,0,0,0],p0));
+    var numerator = Math.abs(this.Dot(normal, point) + b);
+    var denominator = this.Magnitude(normal);
     return numerator / denominator;
   }
 
-  ClassName.prototype.GetFurthestOutsidePointFromFacet = function(facet_i, hull) {
-    var hyperplane = GetHyperplaneOfFacet(hull.facets[facet_i], hull.points);
+  ConvexHull4D.prototype.GetFurthestOutsidePointFromFacet = function(facet_i, hull) {
+    var hyperplane = this.GetHyperplaneOfFacet(hull.facets[facet_i], hull.points);
     var normal = hyperplane[0];
     var p0 = hyperplane[1];
-    var furthest_distance = DistanceFromHyperplane(normal, p0, hull.points[0]);
+    var furthest_distance = this.DistanceFromHyperplane(normal, p0, hull.points[0]);
     var furthest_point_i = 0;
     for (var i = 1; i < hull.points.length; i++) {
-      if (DistanceFromHyperplane(normal, p0, hull.points[i]) > furthest_distance) {
+      if (this.DistanceFromHyperplane(normal, p0, hull.points[i]) > furthest_distance) {
         furthest_point_i = i;
       }
     }
     return furthest_point_i;
   }
 
-  ClassName.prototype.GetRidgesOfFacet = function(facet) {
+  ConvexHull4D.prototype.GetRidgesOfFacet = function(facet) {
   // Following the threejs winding of a tetrahedron.
 
     return [[facet[0], facet[1], facet[2]],
@@ -140,7 +140,7 @@ var ConvexHull4D = (function (scope) {
             [facet[3], facet[1], facet[0]]];
   }
 
-  ClassName.prototype.GetSharedRidges = function(facet_ridges, other_facet_ridges) {
+  ConvexHull4D.prototype.GetSharedRidges = function(facet_ridges, other_facet_ridges) {
     var shared_ridges = [];
     for (var fri = 0; fri < facet_ridges.length; fri++) {
       for (var ofri = 0; ofri < other_facet_ridges.length; ofri++) {
@@ -156,31 +156,31 @@ var ConvexHull4D = (function (scope) {
     return shared_ridges;
   }
 
-  ClassName.prototype.GetNeighborsOfFacet = function(facet_i, hull) {
+  ConvexHull4D.prototype.GetNeighborsOfFacet = function(facet_i, hull) {
     var neighbors = [];
-    var facet_ridges = GetRidgesOfFacet(hull.facets[facet_i]);
+    var facet_ridges = this.GetRidgesOfFacet(hull.facets[facet_i]);
     for (var i = 0; i < hull.facets.length; i++) {
       if (i == facet_i)
         continue;
-      var other_facet_ridges = GetRidgesOfFacet(hull.facets[i]);
-      if (GetSharedRidges(facet_ridges, other_facet_ridges) != [])
+      var other_facet_ridges = this.GetRidgesOfFacet(hull.facets[i]);
+      if (this.GetSharedRidges(facet_ridges, other_facet_ridges) != [])
         neighbors.push(i);
     }
     return neighbors;
   }
 
-  ClassName.prototype.GetHorizonRidges = function(visible_set, point_i, hull) {
+  ConvexHull4D.prototype.GetHorizonRidges = function(visible_set, point_i, hull) {
     var horizon = [];
     for (var visible_set_i = 0; visible_set_i < visible_set.length; visible_set_i++) {
-      var neighbors = GetNeighborsOfFacet(visible_set[visible_set_i], hull);
+      var neighbors = this.GetNeighborsOfFacet(visible_set[visible_set_i], hull);
 
       // If our point of interest is a below a neighboring facet,
       // then the shared ridge ridge with that neighbor is part of the horizon.
       for (var neighbors_i = 0; neighbors_i < neighbors.length; neighbors_i++) {
-        if (!IsPointAboveFacet(neighbors[neighbors_i], point_i, hull)) {
-          var facet_ridges = GetRidgesOfFacet(hull.facets[visible_set[visible_set_i]]);
-          var other_facet_ridges = GetRidgesOfFacet(hull.facets[neighbors[neighbors_i]]);
-          var shared_ridges = GetSharedRidges(facet_ridges, other_facet_ridges);
+        if (!this.IsPointAboveFacet(neighbors[neighbors_i], point_i, hull)) {
+          var facet_ridges = this.GetRidgesOfFacet(hull.facets[visible_set[visible_set_i]]);
+          var other_facet_ridges = this.GetRidgesOfFacet(hull.facets[neighbors[neighbors_i]]);
+          var shared_ridges = this.GetSharedRidges(facet_ridges, other_facet_ridges);
           horizon = horizon.concat(shared_ridges);
         }
       }
@@ -189,7 +189,7 @@ var ConvexHull4D = (function (scope) {
     return horizon;
   }
 
-  ClassName.prototype.RemoveFacetsFromHull = function(facets, hull) {
+  ConvexHull4D.prototype.RemoveFacetsFromHull = function(facets, hull) {
     facets = facets.sort(function(a,b){ return a > b }); // ascending order
     for (var facets_i = facets.length - 1; facets_i >= 0; facets_i--) {
       hull.facets.splice(facets[facets_i], 1);
@@ -198,18 +198,18 @@ var ConvexHull4D = (function (scope) {
     return hull;
   }
 
-  ClassName.prototype.CreateFacetsFromPointAndRidges = function(hull, ridges, point_i) {
+  ConvexHull4D.prototype.CreateFacetsFromPointAndRidges = function(hull, ridges, point_i) {
     for (var ridges_i = 0; ridges_i < ridges.length; ridges_i++) {
       hull.facets.push(ridges[ridges_i].concat(point_i));
     }
     return hull;
   }
 
-  ClassName.prototype.UpdateOutsideSets = function(outside_points_of_visible_set, start_of_new_facets, hull) {
+  ConvexHull4D.prototype.UpdateOutsideSets = function(outside_points_of_visible_set, start_of_new_facets, hull) {
     for (var new_facet_i = start_of_new_facets; new_facet_i < hull.facets.length; new_facet_i++) {
       hull.outside_sets.push([]);
       for (var opi = 0; opi < outside_points_of_visible_set.length; opi++) {
-        if (IsPointAboveFacet(new_facet_i, outside_points_of_visible_set[opi], hull))
+        if (this.IsPointAboveFacet(new_facet_i, outside_points_of_visible_set[opi], hull))
           hull.outside_sets[new_facet_i].push(outside_points_of_visible_set[opi]);
       }
     }
@@ -217,7 +217,7 @@ var ConvexHull4D = (function (scope) {
     return hull;
   }
 
-  ClassName.prototype.GetOutsidePointsOfFacets = function(facets, hull) {
+  ConvexHull4D.prototype.GetOutsidePointsOfFacets = function(facets, hull) {
     var outside_points = [];
     for (var facets_i = 0; facets_i < facets.length; facets_i++) {
       var facet = facets[facets_i];
@@ -231,22 +231,22 @@ var ConvexHull4D = (function (scope) {
     return outside_points;
   }
 
-  ClassName.prototype.ConvexHull4D = function(points) {
+  ConvexHull4D.prototype.ConvexHull4D = function(points) {
     var hull = {};
     hull.points = points;
-    hull.facets = CreateInitialSimplex();
-    hull.outside_sets = GetInitialOutsideSets(hull);
+    hull.facets = this.CreateInitialSimplex();
+    hull.outside_sets = this.GetInitialOutsideSets(hull);
 
     for (var osi = 0; osi < hull.outside_sets.length; osi++) {
       if (hull.outside_sets[osi].length > 0) {
-        var furthest_point_i = GetFurthestOutsidePointFromFacet(osi, hull);
+        var furthest_point_i = this.GetFurthestOutsidePointFromFacet(osi, hull);
         var visible_set = [osi];
-        var neighbors = GetNeighborsOfFacet(visible_set[0], hull);
+        var neighbors = this.GetNeighborsOfFacet(visible_set[0], hull);
 
         for (var neighbors_i = 0; neighbors_i < neighbors.length; neighbors_i++) {
-          if (IsPointAboveFacet(neighbors[neighbors_i], furthest_point_i, hull)) {
+          if (this.IsPointAboveFacet(neighbors[neighbors_i], furthest_point_i, hull)) {
             visible_set.push(neighbors[neighbors_i]);
-            var new_neighbors = GetNeighborsOfFacet(neighbors[neighbors_i], hull);
+            var new_neighbors = this.GetNeighborsOfFacet(neighbors[neighbors_i], hull);
             for (var new_neighbors_i = 0; new_neighbors_i < new_neighbors.length; new_neighbors_i++) {
               if(neighbors.indexOf(new_neighbors[new_neighbors_i]) == -1 && new_neighbors[new_neighbors_i] != osi) {
                 neighbors.push(new_neighbors[new_neighbors_i]);
@@ -255,12 +255,12 @@ var ConvexHull4D = (function (scope) {
           }
         }
 
-        var horizon_ridges = GetHorizonRidges(visible_set, furthest_point_i, hull);
-        var outside_points_of_visible_set  = GetOutsidePointsOfFacets(visible_set, hull);
-        hull = RemoveFacetsFromHull(visible_set, hull);
+        var horizon_ridges = this.GetHorizonRidges(visible_set, furthest_point_i, hull);
+        var outside_points_of_visible_set  = this.GetOutsidePointsOfFacets(visible_set, hull);
+        hull = this.RemoveFacetsFromHull(visible_set, hull);
         var start_of_new_facets = hull.facets.length;
-        hull = CreateFacetsFromPointAndRidges(hull, horizon_ridges, furthest_point_i);
-        hull = UpdateOutsideSets(outside_points_of_visible_set, start_of_new_facets, hull);
+        hull = this.CreateFacetsFromPointAndRidges(hull, horizon_ridges, furthest_point_i);
+        hull = this.UpdateOutsideSets(outside_points_of_visible_set, start_of_new_facets, hull);
 
         osi = 0;
       }
@@ -269,6 +269,26 @@ var ConvexHull4D = (function (scope) {
     return hull.facets;
   }
 
-scope.ClassName = ConvexHull4D;
+scope.ConvexHull4D = ConvexHull4D;
 return ConvexHull4D;
 })(typeof exports === 'undefined' ? {} : exports);
+
+var tesseract = [[0,0,0,0]
+,[1,0,0,0]
+,[0,1,0,0]
+,[1,1,0,0]
+,[0,0,1,0]
+,[1,0,1,0]
+,[0,1,1,0]
+,[1,1,1,0]
+,[0,0,0,1]
+,[1,0,0,1]
+,[0,1,0,1]
+,[1,1,0,1]
+,[1,0,1,1]
+,[0,1,1,1]
+,[1,1,1,1]
+,[0,0,1,1]];
+
+var CHull4D = new ConvexHull4D();
+var facets = CHull4D.ConvexHull4D(tesseract);
