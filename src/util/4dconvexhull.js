b3f6f01b834bc1e932a781be4227904cea1e4d6e
Omar Shehata <shehat1@stolaf.edu>
2017-11-27 21:08:12 -0600

    Updated 4d convexhull in util from demos

--- src/util/4dconvexhull.js
+++ src/util/4dconvexhull.js
@@ -1,5 +1,5 @@
 var ConvexHull4D = (function (scope) {
-	function ConvexHull4D(){}
+  function ConvexHull4D(){}
 
   ConvexHull4D.prototype.arrEqual = function(arr1, arr2) {
     if (arr1.length != arr2.length) {
@@ -20,28 +20,28 @@ var ConvexHull4D = (function (scope) {
 
   ConvexHull4D.prototype.CreateInitialSimplex = function(hull) {
     // Create an initial simplex of d+1 points and return its facets
-		hull.facets.push([1,2,3,4]);
-		if (this.IsPointAboveFacet(0, 0, hull)) {
-			hull.facets[0] = [4,3,2,1];
-		}
-		hull.facets.push([4,3,2,0]);
-		if(this.IsPointAboveFacet(1, 1, hull)) {
-			hull.facets[1] = [0,2,3,4];
-		}
-		hull.facets.push([0,1,3,4]);
-		if(this.IsPointAboveFacet(2, 2, hull)) {
-			hull.facets[2] = [4,3,1,0];
-		}
-		hull.facets.push([4,2,1,0]);
-		if(this.IsPointAboveFacet(3,3, hull)) {
-			hull.facets[3] = [0,1,2,4];
-		}
-		hull.facets.push([0,1,2,3]);
-		if(this.IsPointAboveFacet(4,4, hull)) {
-			hull.facets[4] = [3,2,1,0];
-		}
+    hull.facets.push([1,2,3,4]);
+    if (this.IsPointAboveFacet(0, 0, hull)) {
+      hull.facets[0] = [4,3,2,1];
+    }
+    hull.facets.push([4,3,2,0]);
+    if(this.IsPointAboveFacet(1, 1, hull)) {
+      hull.facets[1] = [0,2,3,4];
+    }
+    hull.facets.push([0,1,3,4]);
+    if(this.IsPointAboveFacet(2, 2, hull)) {
+      hull.facets[2] = [4,3,1,0];
+    }
+    hull.facets.push([4,2,1,0]);
+    if(this.IsPointAboveFacet(3,3, hull)) {
+      hull.facets[3] = [0,1,2,4];
+    }
+    hull.facets.push([0,1,2,3]);
+    if(this.IsPointAboveFacet(4,4, hull)) {
+      hull.facets[4] = [3,2,1,0];
+    }
 
-		return hull;
+    return hull;
   }
 
   ConvexHull4D.prototype.vecSubtract = function(A, B) {
@@ -63,17 +63,17 @@ var ConvexHull4D = (function (scope) {
     // var e2 = A[0]*B[3]*C[1] - A[0]*B[1]*C[3] + A[1]*B[0]*C[3] - A[1]*B[3]*C[0] - A[3]*B[0]*C[1] + A[3]*B[1]*C[0];
     // var e3 = A[0]*B[1]*C[2] - A[0]*B[2]*C[1] - A[1]*B[0]*C[2] + A[1]*B[2]*C[0] + A[2]*B[0]*C[1] - A[2]*B[1]*C[0];
 
-		var A = (V[0] * W[1]) - (V[1] * W[0]);
-		var B = (V[0] * W[2]) - (V[2] * W[0]);
-		var C = (V[0] * W[3]) - (V[3] * W[0]);
-		var D = (V[1] * W[2]) - (V[2] * W[1]);
-		var E = (V[1] * W[3]) - (V[3] * W[1]);
-		var F = (V[2] * W[3]) - (V[3] * W[2]);
- 		// Calculate the result-vector components.
- 		var result0 =   (U[1] * F) - (U[2] * E) + (U[3] * D);
+    var A = (V[0] * W[1]) - (V[1] * W[0]);
+    var B = (V[0] * W[2]) - (V[2] * W[0]);
+    var C = (V[0] * W[3]) - (V[3] * W[0]);
+    var D = (V[1] * W[2]) - (V[2] * W[1]);
+    var E = (V[1] * W[3]) - (V[3] * W[1]);
+    var F = (V[2] * W[3]) - (V[3] * W[2]);
+    // Calculate the result-vector components.
+    var result0 =   (U[1] * F) - (U[2] * E) + (U[3] * D);
     var result1 = - (U[0] * F) + (U[2] * C) - (U[3] * B);
-		var result2 =   (U[0] * E) - (U[1] * C) + (U[3] * A);
-		var result3 = - (U[0] * D) + (U[1] * B) - (U[2] * A);
+    var result2 =   (U[0] * E) - (U[1] * C) + (U[3] * A);
+    var result3 = - (U[0] * D) + (U[1] * B) - (U[2] * A);
     return [result0, result1, result2, result3];
   }
 
@@ -110,7 +110,7 @@ var ConvexHull4D = (function (scope) {
   }
 
   ConvexHull4D.prototype.IsPointAboveFacet = function(facet_index, point_index, hull) {
-		if (hull.points_on_hull.indexOf(point_index) != -1) return false;
+    if (hull.points_on_hull.indexOf(point_index) != -1) return false;
     var hyperplane = this.GetHyperplaneOfFacet(hull.facets[facet_index], hull.points);
     return this.IsPointAboveHyperplane(hyperplane[0], hyperplane[1], hull.points[point_index]);
   }
@@ -248,15 +248,15 @@ var ConvexHull4D = (function (scope) {
     //   }
     // }
 
-		hull.outside_sets = [];
+    hull.outside_sets = [];
 
-		for(var facet_i = 0; facet_i < hull.facets.length; facet_i++) {
-			hull.outside_sets.push([]);
-			for (var opi = 0; opi < hull.points.length; opi++) {
-				if (this.IsPointAboveFacet(facet_i, opi, hull))
-					hull.outside_sets[facet_i].push(opi);
-			}
-		}
+    for(var facet_i = 0; facet_i < hull.facets.length; facet_i++) {
+      hull.outside_sets.push([]);
+      for (var opi = 0; opi < hull.points.length; opi++) {
+        if (this.IsPointAboveFacet(facet_i, opi, hull))
+          hull.outside_sets[facet_i].push(opi);
+      }
+    }
 
     return hull;
   }
@@ -278,13 +278,25 @@ var ConvexHull4D = (function (scope) {
   ConvexHull4D.prototype.ConvexHull4D = function(points) {
     var hull = {};
     hull.points = points;
-		hull.facets = [];
-		hull.points_on_hull = [];
+    hull.facets = [];
+    hull.points_on_hull = [];
     hull = this.CreateInitialSimplex(hull);
-		hull.points_on_hull = [0,1,2,3,4];
+    hull.points_on_hull = [0,1,2,3,4];
     hull.outside_sets = this.GetInitialOutsideSets(hull);
 
-		var counter = 0;
+    /*
+    - This algorithm repeatedly adds a new point to the convex hull of the previously processed points
+    - Each new point is processed in 3 steps:
+      1. Locate visible facets for the point. 
+         * The boundary of these facets is the horizon ridges for the point.
+         * A facet is visible if the point is above the facet.
+      2. Construct a cone of new facets from the point to its horizon ridges.
+      3. Delete the visible facets, thus forming the convex hull of the new point and the previously processed points
+
+      Sounds so easy! 
+     */
+
+    // var counter = 0;
     for (var osi = 0; osi < hull.outside_sets.length; osi++) {
       if (hull.outside_sets[osi].length > 0) {
         var furthest_point_i = this.GetFurthestOutsidePointFromFacet(osi, hull);
@@ -308,13 +320,13 @@ var ConvexHull4D = (function (scope) {
         hull = this.RemoveFacetsFromHull(visible_set, hull);
         var start_of_new_facets = hull.facets.length;
         hull = this.CreateFacetsFromPointAndRidges(hull, horizon_ridges, furthest_point_i);
-				hull.points_on_hull.push(furthest_point_i);
+        hull.points_on_hull.push(furthest_point_i);
         hull = this.UpdateOutsideSets(outside_points_of_visible_set, start_of_new_facets, hull);
 
         osi = 0;
 
-				counter++;
-				if (counter == 7) break;
+        // counter++;
+        // if (counter == 7) break;
       }
     }
 
